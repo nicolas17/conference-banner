7f8e2cb49fd625430079d54e6060109bc4ccc898
Nicolás Alvarez <nicolas.alvarez@gmail.com>
2016-04-04 02:52:55 -0300

    Control content padding using arrow keys.
    Add keyboard shortcuts (arrow keys) to change the CSS padding of the page
    content. This is useful to compensate for TV overscan. It works by
    executing Javascript code that adds new CSS rules to the stylesheet.

--- web.cpp
+++ web.cpp
@@ -8,17 +8,25 @@
 
 #include <QApplication>
 
+#include <QDebug>
+
 #include <QAction>
 #include <QWidget>
 #include <QWebView>
-#include <QDebug>
+#include <QWebFrame>
+#include <QWebElement>
 
 class WebBanner: public QWidget {
 private:
     QWebView* webView;
+    int m_padding = 10;
+    int m_paddingRuleIndex = -1;
 public:
     WebBanner(QApplication* app);
     void resizeEvent(QResizeEvent* event) override;
+
+    void insertPaddingControlRule();
+    void setPadding(int padding);
 };
 
 WebBanner::WebBanner(QApplication* app) {
@@ -39,11 +47,50 @@ WebBanner::WebBanner(QApplication* app) {
     connect(refreshAction, &QAction::triggered, webView, [this] {
         qDebug() << "Refreshing page, requested by keyboard";
         webView->triggerPageAction(QWebPage::Reload);
+        m_paddingRuleIndex = -1;
     });
     this->addAction(refreshAction);
+
+    QAction* increasePaddingAction = new QAction(this);
+    increasePaddingAction->setShortcut(QKeySequence(Qt::Key_Up));
+    connect(increasePaddingAction, &QAction::triggered, this, [this] {
+        setPadding(m_padding+1);
+    });
+    this->addAction(increasePaddingAction);
+
+    QAction* decreasePaddingAction = new QAction(this);
+    decreasePaddingAction->setShortcut(QKeySequence(Qt::Key_Down));
+    connect(decreasePaddingAction, &QAction::triggered, this, [this] {
+        setPadding(m_padding-1);
+    });
+    this->addAction(decreasePaddingAction);
+
+    connect(webView, &QWebView::loadFinished, this, [this] {
+        qDebug() << "Page finished loading";
+        insertPaddingControlRule();
+    });
 }
 void WebBanner::resizeEvent(QResizeEvent* event) {
     webView->resize(this->size());
+}
+void WebBanner::insertPaddingControlRule() {
+    if (m_paddingRuleIndex < 0) {
+        qDebug() << "Adding padding-control CSS";
+        const QString jsCode = QStringLiteral("document.styleSheets[1].insertRule('section { padding: %1px; }', document.styleSheets[1].cssRules.length);").arg(m_padding);
+        qDebug() << "Running " << jsCode;
+        QVariant varRuleIndex = webView->page()->mainFrame()->documentElement().evaluateJavaScript(jsCode);
+        qDebug() << "Result:" << varRuleIndex;
+        m_paddingRuleIndex = varRuleIndex.toInt();
+    }
+
+}
+void WebBanner::setPadding(int padding) {
+    if (m_paddingRuleIndex < 0) return;
+    m_padding = padding;
+    insertPaddingControlRule();
+    const QString jsCode = QStringLiteral("document.styleSheets[1].cssRules[%1].style.padding = '%2px';").arg(m_paddingRuleIndex).arg(m_padding);
+    qDebug() << "Running " << jsCode;
+    QVariant ret = webView->page()->mainFrame()->documentElement().evaluateJavaScript(jsCode);
 }
 
 int main(int argc, char** argv) {
